c19e5d0d44888f0bf3d47a5d476072a0e79810a1
Atsushi <atsushi88885@gmail.com>
2017-08-14 07:39:37 +0900

    change msg

--- web/index.php
+++ web/index.php
@@ -52,7 +52,7 @@ $app->post('/callback', function (Request $request) use ($app) {
                 $res = curl_exec($ch);
                 curl_close($ch);
 
-                $arr = json_decode($res, true);
+                $arr = json_decode($res, false);
 
                 $path = sprintf('me/messages?access_token=%s', getenv('FACEBOOK_PAGE_ACCESS_TOKEN'));
                 $json = [
