c1ba35adfff1b798852ff3d65c62932b40b1dcc0
Atsushi <atsushi88885@gmail.com>
2017-08-16 11:47:40 +0900

    change msg

--- web/index.php
+++ web/index.php
@@ -68,7 +68,7 @@ $app->post('/callback', function (Request $request) use ($app) {
                         $url = $obj->books->$isbn->$system_id->reserveurl;
                         $lend_info = $obj->books->$isbn->$system_id->libkey;
                         foreach ($lend_info as $lib => $status) {
-                            $response = '所蔵館:'.$lib.nl2br('\n').'貸出状況:'.$status.nl2br('\n');
+                            $response = $title.$url.'所蔵館:'.$lib.nl2br('\n').'貸出状況:'.$status.nl2br('\n');
                         }
                     // 図書館に本がなかった場合の処理
                     }else {
